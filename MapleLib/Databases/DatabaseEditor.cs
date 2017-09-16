1ef63cff4dc6f999d2149da5504ef3ac69edfbc3
Jared T <github@tsumes.com>
2017-09-16 15:33:37 -0500

    set DatabaseState

--- MapleLib/Databases/DatabaseEditor.cs
+++ MapleLib/Databases/DatabaseEditor.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MapleLib.Collections;
+using MapleLib.Structs;
 
 namespace MapleLib.Databases
 {
     public class DatabaseEditor
     {
+        private MapleList<Title> DatabaseState;
 
+        public DatabaseEditor()
+        {
+            DatabaseState = new MapleList<Title>(Database.GetLibrary());
+        }
+
+        public void Dispose()
+        {
+            DatabaseState = null;
+        }
     }
 }

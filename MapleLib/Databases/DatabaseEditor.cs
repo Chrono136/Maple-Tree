8d0f8c0c3e22e0c3d17fd63700426f58d68b5318
Jared T <github@tsumes.com>
2017-09-16 16:06:45 -0500

    Basic AddEntry & RemoveEntry

--- MapleLib/Databases/DatabaseEditor.cs
+++ MapleLib/Databases/DatabaseEditor.cs
@@ -12,6 +12,17 @@ namespace MapleLib.Databases
             DatabaseState = new MapleList<Title>(Database.GetLibrary());
         }
 
+        public void AddEntry(Title title)
+        {
+            if (!DatabaseState.Contains(title))
+                DatabaseState.Add(title);
+        }
+
+        public bool RemoveEntry(Title title)
+        {
+            return DatabaseState.Remove(title);
+        }
+
         public void Dispose()
         {
             DatabaseState = null;

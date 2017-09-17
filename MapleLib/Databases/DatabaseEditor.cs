a180315f257b468909d699c802913fc3355ad6a8
Jared T <github@tsumes.com>
2017-09-17 12:55:13 -0500

    Added UserInterfaceManager.cs & EditorForm.cs

--- MapleLib/Databases/DatabaseEditor.cs
+++ MapleLib/Databases/DatabaseEditor.cs
@@ -1,15 +1,19 @@
 ﻿using MapleLib.Collections;
 using MapleLib.Structs;
+using MapleLib.Databases.Editor;
 
 namespace MapleLib.Databases
 {
     public class DatabaseEditor
     {
         private MapleList<Title> DatabaseState;
+        private UserInterfaceManager UIManager;
 
         public DatabaseEditor()
         {
             DatabaseState = new MapleList<Title>(Database.GetLibrary());
+
+            UIManager.Activate();
         }
 
         public void AddEntry(Title title)
---
+++ MapleLib/Databases/Editor/UserInterfaceManager.cs
@@ -0,0 +1,49 @@
+﻿using MapleLib.Databases.Editor.UI;
+using System;
+
+namespace MapleLib.Databases.Editor
+{
+    public class UserInterfaceManager : IDisposable
+    {
+        private EditorForm Form;
+
+        public UserInterfaceManager()
+        {
+            if (Form == null)
+                Form = new EditorForm();
+        }
+
+        public void Activate()
+        {
+            Form.Activate();
+            Form.Show();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Form.Hide();
+                    Form.Dispose();
+                    Form = null;
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}

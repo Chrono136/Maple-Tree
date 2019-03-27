e76ca9eb32af50ba4f0a2f17ddd2f85d6a63732a
Jared T <github@tsumes.com>
2019-03-27 18:08:29 -0500

    ticket changes

--- MapleMini/MapleTicket.cpp
+++ MapleMini/MapleTicket.cpp
@@ -2,7 +2,7 @@
 #include "MapleTicket.h"
 
 
-const int MapleTicket::TICKET_SIZE = 1700;
+const int MapleTicket::TICKET_SIZE = 1500;
 
 MapleTicket::MapleTicket()
 {
@@ -21,12 +21,14 @@ MapleTicket MapleTicket::Create(TitleInfo * ti)
 	{
 		MapleTicket ticket;
 		char buf[TICKET_SIZE];
+		memset(buf, 1, TICKET_SIZE);
 		
 		memcpy(&buf[0x1BF * 2], ti->key, 32);
 		memcpy(&buf[0x1E6 * 2], ti->versions, 2);
 		memcpy(&buf[0x2CC * 2], ti->id, 16);
 
 		auto bytes = HexToBytes(buf);
+		ticket.len = (int)bytes.size();
 		memcpy(ticket.data, bytes.data(), bytes.size());
 
 		return ticket;

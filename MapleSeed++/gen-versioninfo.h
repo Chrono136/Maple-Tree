86cb74049550a583615d7ed61fb0750953557aa8
Jared T <github@tsumes.com>
2019-04-08 12:32:33 -0500

    output working directory prior to DownloadContent

--- MapleSeed++/gen-versioninfo.h
+++ MapleSeed++/gen-versioninfo.h
@@ -1,5 +1,5 @@
 //generated resource header.
 #define GEN_LATEST_VERSION_STRING "0.0.1\0" 
-#define GEN_COMMIT_STRING "2858e787\0" 
-#define GEN_COMMIT_COUNT_STRING "411\0" 
-#define GEN_VERSION_STRING "0.0.1-411-2858e787\0" 
+#define GEN_COMMIT_STRING "1edfa64e\0" 
+#define GEN_COMMIT_COUNT_STRING "412\0" 
+#define GEN_VERSION_STRING "0.0.1-412-1edfa64e\0" 

3b45547c10bb9f6b056046260b136cb6c9aec264
Jared T <github@tsumes.com>
2019-04-08 16:31:10 -0500

    update icon

--- MapleSeed++/gen-versioninfo.h
+++ MapleSeed++/gen-versioninfo.h
@@ -1,5 +1,5 @@
 //generated resource header.
 #define GEN_LATEST_VERSION_STRING "0.0.1\0" 
-#define GEN_COMMIT_STRING "86cb7404\0" 
-#define GEN_COMMIT_COUNT_STRING "413\0" 
-#define GEN_VERSION_STRING "0.0.1-413-86cb7404\0" 
+#define GEN_COMMIT_STRING "da83fa43\0" 
+#define GEN_COMMIT_COUNT_STRING "414\0" 
+#define GEN_VERSION_STRING "0.0.1-414-da83fa43\0" 

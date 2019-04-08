3fbc9877c20cfaca3239765e1a001fb08cc77afa
Jared T <github@tsumes.com>
2019-04-08 10:59:22 -0500

    update CMakeLists.txt

--- MapleSeed++/gen-versioninfo.h
+++ MapleSeed++/gen-versioninfo.h
@@ -1,5 +1,5 @@
 //generated resource header.
 #define GEN_LATEST_VERSION_STRING "0.0.1\0" 
-#define GEN_COMMIT_STRING "9f2ed07a\0" 
-#define GEN_COMMIT_COUNT_STRING "404\0" 
-#define GEN_VERSION_STRING "0.0.1-404-9f2ed07a\0" 
+#define GEN_COMMIT_STRING "ac642b2b\0" 
+#define GEN_COMMIT_COUNT_STRING "407\0" 
+#define GEN_VERSION_STRING "0.0.1-407-ac642b2b\0" 

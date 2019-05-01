6d771263cecf432c71de2e2a347591bb9cdb271b
Tsume <Tsume@users.noreply.github.com>
2019-05-01 11:57:25 -0500

    add versioninfo.h

---
+++ versioninfo.h
@@ -0,0 +1,5 @@
+//generated resource header.
+#define GEN_LATEST_VERSION_STRING "1.0.0\0" 
+#define GEN_COMMIT_STRING "5d705297\0" 
+#define GEN_COMMIT_COUNT_STRING "484\0" 
+#define GEN_VERSION_STRING "1.0.0-484-5d705297\0" 

99cb4f8f658218fd18c4aee9e42292f3f6a67ffc
Tsume <Tsume@users.noreply.github.com>
2019-05-19 23:17:22 -0500

    update solution files

--- versioninfo.h
+++ versioninfo.h
@@ -1,5 +1,5 @@
 //generated resource header.
 #define GEN_LATEST_VERSION_STRING "1.0.0\0" 
-#define GEN_COMMIT_STRING "5d705297\0" 
-#define GEN_COMMIT_COUNT_STRING "484\0" 
-#define GEN_VERSION_STRING "1.0.0-484-5d705297\0" 
+#define GEN_COMMIT_STRING "fd0e96cb\0" 
+#define GEN_COMMIT_COUNT_STRING "491\0" 
+#define GEN_VERSION_STRING "1.0.0-491-fd0e96cb\0" 

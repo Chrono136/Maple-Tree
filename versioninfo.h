884281d6dd941fe0a6aa17083732a1ea011beecc
Tsume <Tsume@users.noreply.github.com>
2019-06-07 08:10:36 -0500

    remove generated files

--- versioninfo.h
+++
@@ -1,5 +0,0 @@
-//generated resource header 
-#define GEN_LATEST_VERSION_STRING "1.0.0\0" 
-#define GEN_COMMIT_STRING "b396cd38\0" 
-#define GEN_COMMIT_COUNT_STRING "552\0" 
-#define GEN_VERSION_STRING "1.0.0.552\0" 
